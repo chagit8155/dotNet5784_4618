1e9d3a06225758865ab05094933fa8f78a5bd474
chagit8155 <cradia@g.jct.ac.il>
2024-01-01 21:56:59 +0200

    Initial Code + Welcome functions

---
+++ Stage0/Program4618.cs
@@ -0,0 +1,20 @@
+﻿namespace Stage0
+{
+    partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            Welcome4618();
+            Console.ReadKey();
+        }
+        static partial void WelcomeYYYY();
+
+        private static void Welcome4618()
+        {
+            Console.WriteLine("Enter your name: ");
+            string? name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to first console application", name);
+
+        }
+    }
+}
