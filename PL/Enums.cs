9b4b1be6e5f7fdf6190b9bf0584703125d609e65
chagit8155 <cradia@g.jct.ac.il>
2024-03-04 15:49:54 +0200

    Adding enum without the option None for an EngineerWindow

--- PL/Enums.cs
+++ PL/Enums.cs
@@ -5,7 +5,22 @@ internal class Enums
 {
 
 }
-internal class EngineerExperienceCollection : IEnumerable
+internal class EngineerExperienceCollection : IEnumerable //enum without the option None
+{
+    static readonly IEnumerable<BO.EngineerExperience> s_enums =
+      (Enum.GetValues(typeof(BO.EngineerExperience)) as IEnumerable<BO.EngineerExperience>)!;
+
+    public IEnumerator GetEnumerator()
+    {
+        foreach (var item in s_enums)
+        {
+            if (item == BO.EngineerExperience.None)
+                continue;
+            yield return item;
+        }
+    }
+}
+internal class FilterEngineerExperienceCollection : IEnumerable
 {
     static readonly IEnumerable<BO.EngineerExperience> s_enums =
       (Enum.GetValues(typeof(BO.EngineerExperience)) as IEnumerable<BO.EngineerExperience>)!;
